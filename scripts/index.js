623cbc4b2d13283ab901188726a59bb179d7f1dd
Ira Grafova <i.grafova@ntc-hitec.ru>
2023-01-07 12:58:06 +0300

    do closeButton

---
+++ scripts/index.js
@@ -0,0 +1,38 @@
+
+const popupElement = document.querySelector('.popup');
+const formEdit = popupElement.querySelector('.edit-form');
+const popupClose = popupElement.querySelector('.edit-form__close');
+const editButton = document.querySelector('.profile-info__edit-button');
+const saveButton = popupElement.querySelector('.edit-form__save');
+let profileNameInput = popupElement.querySelector('#name');
+let profileJobInput = popupElement.querySelector('#job');
+let profileName = document.querySelector('.profile-info__title');
+let profileJob = document.querySelector('.profile-info__subtitle');
+
+//функция закрытия попапа
+const openPopup = function() {
+  popupElement.classList.remove('popup_is-closed');
+}
+const closePopup = function() {
+  popupElement.classList.add('popup_is-closed');
+}
+const closePopupByClickOnOverlay = function(event) {
+  if(event.target === event.currentTarget) {
+    closePopup();
+  }
+}
+editButton.addEventListener('click', openPopup);
+popupClose.addEventListener('click', closePopup);
+
+
+popupElement.addEventListener('click', closePopupByClickOnOverlay);
+
+//функция сохранить
+function handleFormSubmit (evt) {
+  evt.preventDefault();
+  profileName.textContent =  profileNameInput.value;
+  profileJob.textContent = profileJobInput.value;
+
+  closePopup ();
+}
+formEdit.addEventListener('submit', handleFormSubmit);
