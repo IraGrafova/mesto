980009e76704c965e9000cebe74816a7a4e7ccae
Ira Grafova <i.grafova@ntc-hitec.ru>
2023-01-21 15:09:34 +0300

    new elements

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,30 @@
 
 const popupElement = document.querySelector('.popup');
-const formEdit = popupElement.querySelector('.edit-form');
+const popupEditProfile = document.querySelector('.popup_type_edit-profile');
+const popupAddPicture = document.querySelector('.popup_type_add-picture');
+const popupOpenPicture = document.querySelector('.popup_type_open-picture');
+const closePopupEditProfile = document.querySelector('.popup__close_type_edit-profile');
+const closePopupAddPicture = document.querySelector('.popup__close_type_add-picture');
+const closePopupOpenPicture = document.querySelector('.popup__close_type_open-picture');
+
+console.log(popupEditProfile, closePopupEditProfile, popupAddPicture, closePopupAddPicture, popupOpenPicture, closePopupOpenPicture);
+
+const formEdit = popupElement.querySelector('.card-form');
 const popupClose = popupElement.querySelector('.popup__close');
 const editButton = document.querySelector('.profile-info__edit-button');
-const saveButton = popupElement.querySelector('.edit-form__save');
+const saveButton = popupElement.querySelector('.card-form__save');
 let profileNameInput = popupElement.querySelector('#name');
 let profileJobInput = popupElement.querySelector('#job');
+let placeNameInput = popupElement.querySelector('#place');
+let placeLinkInput = popupElement.querySelector('#place-link');
 let profileName = document.querySelector('.profile-info__title');
 let profileJob = document.querySelector('.profile-info__subtitle');
 
 //функция закрытия попапа
-const openPopup = function() {
-  popupElement.classList.add('popup_is-opened');
+const openPopup = function (popup) {
+  popup.classList.add('popup_is-opened');
+
+  //popupElement.classList.add('popup_is-opened');
   profileNameInput.value = profileName.textContent;
   profileJobInput.value = profileJob.textContent;
 }
